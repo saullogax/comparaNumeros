9d8a7897e813896eb878adad5d34a5a0a9d79e86
Guillermo Gaxiola <saullogax@gmail.com>
2024-07-20 18:16:57 -0700

    Compara 2, 3 y 4 numeros

---
+++ CompararDosNumeros/Form1.Designer.vb
@@ -0,0 +1,387 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnComparar1 = New System.Windows.Forms.Button()
+        Me.txtNum1 = New System.Windows.Forms.TextBox()
+        Me.txtNum2 = New System.Windows.Forms.TextBox()
+        Me.lblR = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.btnComparar2 = New System.Windows.Forms.Button()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.txtNum3 = New System.Windows.Forms.TextBox()
+        Me.txtNum5 = New System.Windows.Forms.TextBox()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.txtNum4 = New System.Windows.Forms.TextBox()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.lblR2 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtNum6 = New System.Windows.Forms.TextBox()
+        Me.txtNum8 = New System.Windows.Forms.TextBox()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.txtNum7 = New System.Windows.Forms.TextBox()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.Label13 = New System.Windows.Forms.Label()
+        Me.Label14 = New System.Windows.Forms.Label()
+        Me.txtNum9 = New System.Windows.Forms.TextBox()
+        Me.btnComparar3 = New System.Windows.Forms.Button()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'btnComparar1
+        '
+        Me.btnComparar1.Location = New System.Drawing.Point(200, 229)
+        Me.btnComparar1.Name = "btnComparar1"
+        Me.btnComparar1.Size = New System.Drawing.Size(94, 23)
+        Me.btnComparar1.TabIndex = 0
+        Me.btnComparar1.Text = "COMPARAR"
+        Me.btnComparar1.UseVisualStyleBackColor = True
+        '
+        'txtNum1
+        '
+        Me.txtNum1.Location = New System.Drawing.Point(200, 86)
+        Me.txtNum1.Name = "txtNum1"
+        Me.txtNum1.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum1.TabIndex = 1
+        '
+        'txtNum2
+        '
+        Me.txtNum2.Location = New System.Drawing.Point(200, 131)
+        Me.txtNum2.Name = "txtNum2"
+        Me.txtNum2.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum2.TabIndex = 1
+        '
+        'lblR
+        '
+        Me.lblR.AutoSize = True
+        Me.lblR.Location = New System.Drawing.Point(164, 197)
+        Me.lblR.Name = "lblR"
+        Me.lblR.Size = New System.Drawing.Size(19, 13)
+        Me.lblR.TabIndex = 2
+        Me.lblR.Text = "¿?"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(18, 89)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Numero uno:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(19, 134)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Numero dos:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(45, 28)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(249, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Comparamos dos numeros y determinamos el mayor"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(18, 197)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(99, 13)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "El numeo mayor es:"
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.btnComparar2)
+        Me.GroupBox1.Controls.Add(Me.Label8)
+        Me.GroupBox1.Controls.Add(Me.txtNum3)
+        Me.GroupBox1.Controls.Add(Me.txtNum5)
+        Me.GroupBox1.Controls.Add(Me.Label9)
+        Me.GroupBox1.Controls.Add(Me.txtNum4)
+        Me.GroupBox1.Controls.Add(Me.Label7)
+        Me.GroupBox1.Controls.Add(Me.lblR2)
+        Me.GroupBox1.Controls.Add(Me.Label6)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 258)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(357, 225)
+        Me.GroupBox1.TabIndex = 7
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Comparar tres numeros"
+        '
+        'btnComparar2
+        '
+        Me.btnComparar2.Location = New System.Drawing.Point(188, 196)
+        Me.btnComparar2.Name = "btnComparar2"
+        Me.btnComparar2.Size = New System.Drawing.Size(94, 23)
+        Me.btnComparar2.TabIndex = 0
+        Me.btnComparar2.Text = "COMPARAR"
+        Me.btnComparar2.UseVisualStyleBackColor = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(18, 146)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(99, 13)
+        Me.Label8.TabIndex = 6
+        Me.Label8.Text = "El numeo mayor es:"
+        '
+        'txtNum3
+        '
+        Me.txtNum3.Location = New System.Drawing.Point(200, 35)
+        Me.txtNum3.Name = "txtNum3"
+        Me.txtNum3.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum3.TabIndex = 1
+        '
+        'txtNum5
+        '
+        Me.txtNum5.Location = New System.Drawing.Point(200, 87)
+        Me.txtNum5.Name = "txtNum5"
+        Me.txtNum5.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum5.TabIndex = 1
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(19, 90)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(67, 13)
+        Me.Label9.TabIndex = 4
+        Me.Label9.Text = "Numero tres:"
+        '
+        'txtNum4
+        '
+        Me.txtNum4.Location = New System.Drawing.Point(200, 61)
+        Me.txtNum4.Name = "txtNum4"
+        Me.txtNum4.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum4.TabIndex = 1
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(19, 64)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(67, 13)
+        Me.Label7.TabIndex = 4
+        Me.Label7.Text = "Numero dos:"
+        '
+        'lblR2
+        '
+        Me.lblR2.AutoSize = True
+        Me.lblR2.Location = New System.Drawing.Point(164, 146)
+        Me.lblR2.Name = "lblR2"
+        Me.lblR2.Size = New System.Drawing.Size(19, 13)
+        Me.lblR2.TabIndex = 2
+        Me.lblR2.Text = "¿?"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(18, 38)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(68, 13)
+        Me.Label6.TabIndex = 3
+        Me.Label6.Text = "Numero uno:"
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.Label12)
+        Me.GroupBox2.Controls.Add(Me.btnComparar3)
+        Me.GroupBox2.Controls.Add(Me.Label5)
+        Me.GroupBox2.Controls.Add(Me.txtNum6)
+        Me.GroupBox2.Controls.Add(Me.txtNum9)
+        Me.GroupBox2.Controls.Add(Me.Label14)
+        Me.GroupBox2.Controls.Add(Me.txtNum8)
+        Me.GroupBox2.Controls.Add(Me.Label10)
+        Me.GroupBox2.Controls.Add(Me.txtNum7)
+        Me.GroupBox2.Controls.Add(Me.Label11)
+        Me.GroupBox2.Controls.Add(Me.Label13)
+        Me.GroupBox2.Location = New System.Drawing.Point(379, 28)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(357, 225)
+        Me.GroupBox2.TabIndex = 7
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Comparar tres numeros"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(18, 146)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(99, 13)
+        Me.Label5.TabIndex = 6
+        Me.Label5.Text = "El numeo mayor es:"
+        '
+        'txtNum6
+        '
+        Me.txtNum6.Location = New System.Drawing.Point(200, 35)
+        Me.txtNum6.Name = "txtNum6"
+        Me.txtNum6.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum6.TabIndex = 1
+        '
+        'txtNum8
+        '
+        Me.txtNum8.Location = New System.Drawing.Point(200, 87)
+        Me.txtNum8.Name = "txtNum8"
+        Me.txtNum8.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum8.TabIndex = 1
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(19, 90)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(67, 13)
+        Me.Label10.TabIndex = 4
+        Me.Label10.Text = "Numero tres:"
+        '
+        'txtNum7
+        '
+        Me.txtNum7.Location = New System.Drawing.Point(200, 61)
+        Me.txtNum7.Name = "txtNum7"
+        Me.txtNum7.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum7.TabIndex = 1
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(19, 64)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(67, 13)
+        Me.Label11.TabIndex = 4
+        Me.Label11.Text = "Numero dos:"
+        '
+        'Label13
+        '
+        Me.Label13.AutoSize = True
+        Me.Label13.Location = New System.Drawing.Point(18, 38)
+        Me.Label13.Name = "Label13"
+        Me.Label13.Size = New System.Drawing.Size(68, 13)
+        Me.Label13.TabIndex = 3
+        Me.Label13.Text = "Numero uno:"
+        '
+        'Label14
+        '
+        Me.Label14.AutoSize = True
+        Me.Label14.Location = New System.Drawing.Point(19, 116)
+        Me.Label14.Name = "Label14"
+        Me.Label14.Size = New System.Drawing.Size(80, 13)
+        Me.Label14.TabIndex = 4
+        Me.Label14.Text = "Numero cuatro:"
+        '
+        'txtNum9
+        '
+        Me.txtNum9.Location = New System.Drawing.Point(200, 113)
+        Me.txtNum9.Name = "txtNum9"
+        Me.txtNum9.Size = New System.Drawing.Size(100, 20)
+        Me.txtNum9.TabIndex = 1
+        '
+        'btnComparar3
+        '
+        Me.btnComparar3.Location = New System.Drawing.Point(200, 180)
+        Me.btnComparar3.Name = "btnComparar3"
+        Me.btnComparar3.Size = New System.Drawing.Size(94, 23)
+        Me.btnComparar3.TabIndex = 8
+        Me.btnComparar3.Text = "COMPARAR"
+        Me.btnComparar3.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(200, 146)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(45, 13)
+        Me.Label12.TabIndex = 9
+        Me.Label12.Text = "Label12"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(739, 495)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lblR)
+        Me.Controls.Add(Me.txtNum2)
+        Me.Controls.Add(Me.txtNum1)
+        Me.Controls.Add(Me.btnComparar1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.GroupBox2.ResumeLayout(False)
+        Me.GroupBox2.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnComparar1 As System.Windows.Forms.Button
+    Friend WithEvents txtNum1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum2 As System.Windows.Forms.TextBox
+    Friend WithEvents lblR As System.Windows.Forms.Label
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents btnComparar2 As System.Windows.Forms.Button
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents txtNum3 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum5 As System.Windows.Forms.TextBox
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents txtNum4 As System.Windows.Forms.TextBox
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents lblR2 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents txtNum6 As System.Windows.Forms.TextBox
+    Friend WithEvents txtNum9 As System.Windows.Forms.TextBox
+    Friend WithEvents Label14 As System.Windows.Forms.Label
+    Friend WithEvents txtNum8 As System.Windows.Forms.TextBox
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents txtNum7 As System.Windows.Forms.TextBox
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+    Friend WithEvents Label13 As System.Windows.Forms.Label
+    Friend WithEvents btnComparar3 As System.Windows.Forms.Button
+    Friend WithEvents Label12 As System.Windows.Forms.Label
+
+End Class
---
+++ CompararDosNumeros/Form1.vb
@@ -0,0 +1,68 @@
+﻿Public Class Form1
+
+    
+    Private Sub btnComparar1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnComparar1.Click
+        If Val(txtNum1.Text) > Val(txtNum2.Text) Then
+            lblR.Text = "El numero mayor es: " & Val(txtNum1.Text)
+        Else
+            lblR.Text = "El numero mayor es: " & Val(txtNum2.Text)
+        End If
+    End Sub
+
+    Private Sub btnComparar2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnComparar2.Click
+        If Val(txtNum3.Text) > Val(txtNum4.Text) Then
+            If Val(txtNum3.Text) > Val(txtNum5.Text) Then
+                lblR2.Text = "El numero mayor es: " & txtNum3.Text
+            Else
+                lblR2.Text = "El numero mayor es: " & txtNum5.Text
+            End If
+        Else
+            If Val(txtNum4.Text) > Val(txtNum5.Text) Then
+                lblR2.Text = "El numero mayor es: " & txtNum4.Text
+            Else
+                lblR2.Text = "El numero mayor es: " & txtNum5.Text
+            End If
+        End If
+    End Sub
+
+    Private Sub btnComparar3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnComparar3.Click
+        If Val(txtNum6.Text) > (Val(txtNum7.Text)) And (Val(txtNum6.Text)) > (Val(txtNum8.Text)) And (Val(txtNum6.Text)) > (Val(txtNum9.Text)) Then
+            Label12.Text = "es mayor " + txtNum6.Text
+        End If
+        If Val(txtNum7.Text) > (Val(txtNum6.Text)) And (Val(txtNum7.Text)) > (Val(txtNum8.Text)) And (Val(txtNum7.Text)) > (Val(txtNum9.Text)) Then
+            Label12.Text = "es mayor " + txtNum7.Text
+        End If
+        If Val(txtNum8.Text) > (Val(txtNum6.Text)) And (Val(txtNum8.Text)) > (Val(txtNum7.Text)) And (Val(txtNum8.Text)) > (Val(txtNum9.Text)) Then
+            Label12.Text = "es mayor " + txtNum8.Text
+        End If
+        If Val(txtNum9.Text) > (Val(txtNum6.Text)) And (Val(txtNum9.Text)) > (Val(txtNum7.Text)) And (Val(txtNum9.Text)) > (Val(txtNum8.Text)) Then
+            Label12.Text = "es mayor " + txtNum9.Text
+        End If
+
+
+    End Sub
+    'If Val(txtNum6.Text) > Val(txtNum7.Text) Then
+    '      If Val(txtNum6.Text) > Val(txtNum8.Text) Then
+    '          If Val(txtNum6.Text) > Val(txtNum9.Text) Then
+    '              Label12.Text = "El numero mayor es: " + txtNum6.Text
+    '          Else
+    '              Label12.Text = "El numero mayor es: " + txtNum9.Text
+    '          End If
+    '      Else
+    '          If Val(txtNum7.Text) > Val(txtNum8.Text) Then
+    '              Label12.Text = "El numero mayor es: " + txtNum7.Text
+    '          Else
+    '              Label12.Text = "El numero mayor es: " + txtNum8.Text
+    '          End If
+
+    '          If Val(txtNum8.Text) > Val(txtNum9.Text) Then
+    '              Label12.Text = "El numero mayor es: " + txtNum8.Text
+    '          Else
+    '              Label12.Text = "El numero mayor es: " + txtNum9.Text
+
+
+    '          End If
+
+    '          End If
+    '      End If
+End Class
